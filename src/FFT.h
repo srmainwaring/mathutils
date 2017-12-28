f0380d428e1ccf4bd760addd09d1f10e71147274
François Rongère <Francois.Rongere@ec-nantes.fr>
2017-12-28 12:00:50 +0100

    Itegrating FFT from Eigen (usig KissFFT)

--- src/FFT.h
+++ src/FFT.h
@@ -5,52 +5,95 @@
 #ifndef MATHUTILS_FFT_H
 #define MATHUTILS_FFT_H
 
-#include "kiss_fft.h"
+//#include "kiss_fft.h"
 
+#include <unsupported/Eigen/FFT>
 
 namespace mathutils {
 
-    template <class Real>
-    class FFT {
-
-    private:
-        kiss_fft_cfg     m_fft_cfg;
-
-//        unsigned int NFFT;
-//        bool Inverse;
-
+    template <class Scalar>
+    class FFT : protected Eigen::FFT<Scalar> {
 
     public:
-        FFT() {
 
-//            kiss_fft_alloc();
-
+        void fft(std::vector<std::complex<Scalar>>& freqVector, const std::vector<Scalar>& vector) {
+            this->fwd(freqVector, vector);
         }
 
-        FFT(unsigned int NFFT) {
-            SetNbPoints(NFFT);
+        void ifft(std::vector<Scalar>& vector, const std::vector<std::complex<Scalar>>& freqVector) {
+            this->inv(vector, freqVector);
         }
 
-        ~FFT() {
-            std::cout << "Destroying FFT object" << std::endl;
-            kiss_fft_free(m_fft_cfg);
+        void HalfSpectrumON() {
+            this->SetFlag(Eigen::FFT<Scalar>::HalfSpectrum);
+//            this->HalfSpectrum = true;
         }
 
-        void SetNbPoints(unsigned int NFFT) {
-            m_fft_cfg = kiss_fft_alloc(NFFT, 0, 0, 0);
+        void HalfSpectrumOFF() {
+            this->ClearFlag(Eigen::FFT<Scalar>::HalfSpectrum);
+//            this->HalfSpectrum = false;
         }
 
-
-
-        std::vector<Real> fft(const std::vector<Real>& f) {
-
+        void ScalingON() {
+            this->ClearFlag(Eigen::FFT<Scalar>::Unscaled);
+//            this->Unscaled = false;
         }
 
-
-
-
+        void ScalingOFF() {
+            this->SetFlag(Eigen::FFT<Scalar>::Unscaled);
+//            this->Unscaled = true;
+        }
 
     };
+
+
+    
+
+
+
+
+
+//    template <class Real>
+//    class FFT {
+//
+//    private:
+//        kiss_fft_cfg     m_fft_cfg;
+//
+////        unsigned int NFFT;
+////        bool Inverse;
+//
+//
+//    public:
+//        FFT() {
+//
+////            kiss_fft_alloc();
+//
+//        }
+//
+//        FFT(unsigned int NFFT) {
+//            SetNbPoints(NFFT);
+//        }
+//
+//        ~FFT() {
+//            std::cout << "Destroying FFT object" << std::endl;
+//            kiss_fft_free(m_fft_cfg);
+//        }
+//
+//        void SetNbPoints(unsigned int NFFT) {
+//            m_fft_cfg = kiss_fft_alloc(NFFT, 0, 0, 0);
+//        }
+//
+//
+//
+//        std::vector<Real> fft(const std::vector<Real>& f) {
+//
+//        }
+//
+//
+//
+//
+//
+//    };
 
 
 
--- tests/test_fft.cpp
+++ tests/test_fft.cpp
@@ -6,15 +6,67 @@
 
 using namespace mathutils;
 
+
+// TODO: a mettre dans FFT.h
+unsigned int NextPow2(const unsigned int n, const bool returnValue=true) {
+    unsigned int p = 0;
+    unsigned int val;
+    while (true) {
+        val = (unsigned int)pow(2, p);
+        if (val >= n) {
+            if (returnValue) {
+                return val;
+            } else {
+                return p;
+            }
+        }
+        p++;
+    }
+}
+
+
+
+
+template <class Scalar>
+std::vector<Scalar> MakeDataSin(const std::vector<Scalar>& timeVector) {
+
+    auto len = timeVector.size();
+
+    std::vector<Scalar> vector;
+    vector.reserve(len);
+
+    for (auto t: timeVector) {
+        vector.push_back(sin(t));
+    }
+
+    return vector;
+
+}
+
+
 int main(int argc, char* argv[]) {
 
-    FFT<double> a(1024);
+
+    // Building data
+    auto time = linspace<double>(0, 2.*MU_2PI, NextPow2(1000));
+
+    auto timeVector = MakeDataSin<double>(time);
 
 
-//    FFT myFFT<double>;
-//    kissfft<double> a();
+    // Building the fft object
+    FFT<double> fft;
+    fft.ScalingON();
+    fft.HalfSpectrumON();
 
+    // Computing FFT
+    std::vector<std::complex<double>> freqVect;
+    fft.fft(freqVect, timeVector);
 
+    // Computing IFFT from spectrum
+    std::vector<double> timeVectRebuild;
+    fft.ifft(timeVectRebuild, freqVect);
+
+    AssertIsClose(timeVector, timeVectRebuild);
 
     return 0;
 }
