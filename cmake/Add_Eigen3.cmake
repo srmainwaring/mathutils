143780d9297f389b934d13d1ad523763a3ce9410
François Rongère <Francois.Rongere@ec-nantes.fr>
2018-04-25 15:49:09 +0200

    Dealing with eigen dependency

---
+++ cmake/Add_Eigen3.cmake
@@ -0,0 +1,23 @@
+find_package(Eigen3 QUIET)
+if (NOT Eigen3_FOUND)
+    include(FetchContent)
+
+    set(Eigen3_URL http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz)
+    FetchContent_Declare(Eigen3
+            URL ${Eigen3_URL}
+            )
+
+    FetchContent_GetProperties(Eigen3)
+    if(NOT Eigen3_POPULATED)
+        message(STATUS "Downloading, Configuring and Generating 'Eigen3' dependency")
+        FetchContent_Populate(Eigen3)
+
+        # Eigen3 BUILD OPTIONS
+        # NONE
+
+        message(Eigen3_SOURCE_DIR${eigen3_SOURCE_DIR})
+        message(Eigen3_BINARY_DIR${eigen3_BINARY_DIR})
+
+        add_subdirectory(${eigen3_SOURCE_DIR} ${eigen3_BINARY_DIR})
+    endif()
+endif()
