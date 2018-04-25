446b816d0be87201c7dabc171913a76f1e93d243
François Rongère <Francois.Rongere@ec-nantes.fr>
2018-04-25 16:11:34 +0200

    Renaming

---
+++ src/MathUtils/MathUtils.h
@@ -0,0 +1,36 @@
+//
+// Created by frongere on 15/11/17.
+//
+
+#ifndef MATHUTILS_H_H
+#define MATHUTILS_H_H
+
+
+#include "Eigen/Dense"
+
+//#include "ceres/ceres.h"
+
+#include "Constants.h"
+#include "Vector2d.h"
+#include "Vector3d.h"
+#include "Unit.h"
+#include "Angles.h"
+#include "Maths.h"
+#include "Interp1d.h"
+#include "LookupTable1D.h"
+#include "Interp2d.h"
+#include "LookupTable2D.h"
+#include "VectorGeneration.h"
+#include "Check.h"
+#include "Transform3d.h"
+#include "Matrix.h"
+#include "VectorN.h"
+#include "Integration1d.h"
+#include "FFT.h"
+#include "Complex.h"
+#include "Functions.h"
+#include "Spline.h"
+#include "StdVector.h"
+#include "Convolution.h"
+
+#endif //FRYDOM_MATHUTILS_H_H
--- src/mathutils/Version.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by frongere on 16/11/17.
-//
-
-#include "Version.h"
