22fe2c62eb4a5988579bfd576bee61ebf74511fa
François Rongère <Francois.Rongere@ec-nantes.fr>
2018-04-26 11:35:31 +0200

    Fix

--- cmake/Add_Eigen3.cmake
+++ cmake/Add_Eigen3.cmake
@@ -19,9 +19,13 @@ if (NOT Eigen3_FOUND)
         message("Eigen3_SOURCE_DIR = ${eigen3_SOURCE_DIR}")
         message("Eigen3_BINARY_DIR = ${eigen3_BINARY_DIR}")
 
-        add_subdirectory(${eigen3_SOURCE_DIR} ${eigen3_BINARY_DIR})
-#        add_subdirectory(${eigen3_SOURCE_DIR})
-#        add_subdirectory(${eigen3_BINARY_DIR})
+        #// add_subdirectory(${eigen3_SOURCE_DIR} ${eigen3_BINARY_DIR})
+        
+        message(STATUS "add_subdirectory(${eigen3_SOURCE_DIR})")
+        add_subdirectory(${eigen3_SOURCE_DIR})
+        
+        message(STATUS "add_subdirectory(${eigen3_BINARY_DIR}")
+        add_subdirectory(${eigen3_BINARY_DIR})
     endif()
 endif()
 
