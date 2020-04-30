f3920079f295f49ff712c4a1f6ab042e277a94c7
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2020-04-30 10:46:38 +0200

    Update test_linear_system_solver.

--- tests/test_linear_system_solver.cpp
+++ tests/test_linear_system_solver.cpp
@@ -2,16 +2,24 @@
 // Created by pierre-yves on 22/10/19.
 //
 
-#include <iostream>
 #include "MathUtils/MathUtils.h"
 
-#include <iostream>
-#include <Eigen/Dense>
-using namespace std;
-using namespace Eigen;
+using namespace mathutils;
+
+void PrintHeader(std::string title) {
+  std::cout << "\n=====================================================================" << std::endl;
+  std::cout << "    " << title << std::endl;
+  std::cout << "=====================================================================" << std::endl;
+}
+
+void PrintInfo(std::string info) {
+  std::cout << info << ":" << std::endl;
+}
 
 
 int main(int argc, char* argv[]) {
+
+  PrintHeader("Matrix66");
 
   // Matrix66.
   mathutils::Matrix66<double> A66;
@@ -22,40 +30,79 @@ int main(int argc, char* argv[]) {
        1,7,9,3,-20,4,
        0,0,3,4,8,9;
 
+  PrintInfo("The matrix A");
+  std::cout << A66 << "\n\n";
+
   mathutils::Vector6d<double> b6;
   b6 << 1, 2, 3, 4, 5, 6;
 
-  auto x6 = A66.LUSolver<mathutils::Vector6d<double>, mathutils::Vector6d<double>>(b6);
+  PrintInfo("The right-hand side vector b");
+  std::cout << b6 << "\n\n";
 
-  std::cout << "" << std::endl;
-  std::cout << x6 << std::endl;
+  auto x6 = A66.LUSolver(b6);
+
+  PrintInfo("The solution x from a LU decomposition");
+  std::cout << x6 << "\n\n";
+
+  PrintInfo("Verification (A * x - b = 0)");
+  std::cout << A66 * x6 - b6 << "\n\n";
+  Vector6d<double> Ax66 = A66 * x6;
+  assert(Ax66.IsEqual(b6));
 
   // Matrix33.
+
+  PrintHeader("Matrix33");
+
   mathutils::Matrix33<double> A33;
   A33 << 2,5,7,
       3,7,-4,
       -10,13,24;
 
+  PrintInfo("The matrix A");
+  std::cout << A33 << "\n\n";
+
   mathutils::Vector3d<double> b3;
   b3 << 1, 2, 3;
 
-  auto x3 = A33.LUSolver<mathutils::Vector3d<double>, mathutils::Vector3d<double>>(b3);
+  PrintInfo("The right-hand side vector b");
+  std::cout << b3 << "\n\n";
 
-  std::cout << "" << std::endl;
-  std::cout << x3 << std::endl;
+  auto x3 = A33.LUSolver(b3);
+
+  PrintInfo("The solution x from a LU decomposition");
+  std::cout << x3 << "\n\n";
+
+  PrintInfo("Verification (A * x - b = 0)");
+  std::cout << A33 * x3 - b3 << "\n\n";
+  Vector3d<double> Ax33 = A33 * x3;
+  assert(Ax33.IsEqual(b3));
 
   // MatrixMN.
+
+  PrintHeader("MatrixMN");
+
   mathutils::MatrixMN<double> AMN = mathutils::MatrixMN<double>::Zero(6, 6);
   AMN = A66;
+
+  PrintInfo("The matrix A");
+  std::cout << A66 << "\n\n";
 
   mathutils::MatrixMN<double> bN = mathutils::MatrixMN<double>::Zero(6, 2);
   bN << 1, 1, 2, 2, 3, 3,
       4, 4, 5, 5, 6, 6;
 
-  auto xN = AMN.LUSolver<mathutils::MatrixMN<double>, mathutils::MatrixMN<double>>(bN);
+  PrintInfo("The right-hand side vector b");
+  std::cout << bN << "\n\n";
 
-  std::cout << "" << std::endl;
-  std::cout << xN << std::endl;
+  auto xN = AMN.LUSolver(bN);
+
+  PrintInfo("The solution x from a LU decomposition");
+  std::cout << xN << "\n\n";
+
+  PrintInfo("Verification (A * x - b = 0)");
+  std::cout << AMN * xN - bN << "\n\n";
+  mathutils::MatrixMN<double> AxMN = AMN * xN;
+  assert(AxMN.IsEqual(bN));
 
   return 0;
 }
