611671321ee0d9fbbb517fb9cb148f93c28a4890
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2020-12-01 17:17:05 +0100

    Refacto of test_integration1d.

--- tests/test_integration1d.cpp
+++ tests/test_integration1d.cpp
@@ -6,41 +6,41 @@
 
 using namespace mathutils;
 
-
-// Defining a function to integrate
-double myFunction(double x) {
-    if (x == 0.) {
-        return 1.;
-    } else {
-        return sin(x) / x;
-    }
+void PrintHeader(std::string title) {
+  std::cout << "\n=====================================================================" << std::endl;
+  std::cout << "    " << title << std::endl;
+  std::cout << "=====================================================================" << std::endl;
 }
 
+// Definition of a function to integrate.
+double myFunction(double x) {
+  if (x == 0.) {
+      return 1.;
+  } else {
+      return sin(x) / x;
+  }
+}
 
 int main(int argc, char* argv[]) {
 
-    // Defining the integration.
-    // Here, by default we use the trapezoidal method
-    auto myIntegrator = Integrate1d<double>(myFunction, 0, 10, 100);
-    std::cout << myIntegrator.Get() << std::endl;
+  // This test checks the numerical integration of a one-variable function.
 
+  PrintHeader("Computation of the integral of sinc(x) between 0 and 1.");
+  std::cout << "Analytical: 0.946083" << std::endl;
 
-    // Defining a vector
-    auto x = linspace<double>(0, 10, 100);
-    std::vector<double> y;
-    for (auto val : x) {
-        y.push_back(myFunction(val));
-    }
+  // Defining the integration.
+  // Here, by default we use the trapezoidal method.
+  auto myIntegrator = Integrate1d<double>(myFunction, 0, 1, 1000);
+  std::cout << "Integration by a function: " << myIntegrator.Get() << std::endl;
 
-    auto myIntegratorVect = Integrate1d<double>(y, 0, 10, 100);
-    std::cout << myIntegratorVect.Get() << std::endl;
+  // Defining a vector.
+  auto x = linspace<double>(0, 1, 1000);
+  std::vector<double> y;
+  for (auto val : x) {
+      y.push_back(myFunction(val));
+  }
+  auto myIntegratorVect = Integrate1d<double>(y, 0, 1, 1000);
+  std::cout << "Integration by a vector: " << myIntegratorVect.Get() << std::endl;
 
-
-
-
-
-
-
-
-    return 0;
+  return 0;
 }
