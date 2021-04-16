3f1f510fede84cbd6a6d341aeb163c27a8203021
François Rongère <francois.rongere@dice-engineering.com>
2021-04-16 16:59:50 +0200

    Removing warning about non return type

--- src/MathUtils/Unit.h
+++ src/MathUtils/Unit.h
@@ -19,40 +19,41 @@
 namespace mathutils {
 
 
-    /* ===========================================================================================
-     * ANGLES Conversions functions
-     */
+  /* ===========================================================================================
+   * ANGLES Conversions functions
+   */
 
-    enum ANGLE_UNIT {
-        DEG,
-        RAD
-    };
+  enum ANGLE_UNIT {
+    DEG,
+    RAD
+  };
 
-    /// Conversion DEG->RAD inplace
-    template <class T>
-    inline void deg2rad(std::vector<T>& vector) {
-        for (auto& v : vector) v *= DEG2RAD;
-    }
+  /// Conversion DEG->RAD inplace
+  template<class T>
+  inline void deg2rad(std::vector<T> &vector) {
+    for (auto &v : vector) v *= DEG2RAD;
+  }
 
-    /// CONVERSION DEG->RAD by copy
-    template <class T>
-    inline void deg2rad(const std::vector<T>& vector, std::vector<T>& out) {
-        out = vector;
-        return deg2rad(out);
-    }
+  /// CONVERSION DEG->RAD by copy
+  template<class T>
+  inline void deg2rad(const std::vector<T> &vector, std::vector<T> &out) {
+    out = vector;
+    return deg2rad(out);
+  }
 
-    /// Conversion RAD->DEG inplace
-    template <class T>
-    inline void rad2deg(std::vector<T>& vector) {
-        for (auto& v : vector) v *= RAD2DEG;
-    }
+  /// Conversion RAD->DEG inplace
+  template<class T>
+  inline void rad2deg(std::vector<T> &vector) {
+    for (auto &v : vector) v *= RAD2DEG;
+  }
 
-    /// CONVERSION RAD->DEG by copy
-    template <class T> // TODO : faire la meme inplace...
-    inline std::vector<T> rad2deg(const std::vector<T>& vector, std::vector<T>& out) {
-        out = vector;
-        rad2deg(vector);
-    }
+  /// CONVERSION RAD->DEG by copy
+  template<class T>
+  // TODO : faire la meme inplace...
+  inline std::vector<T> rad2deg(const std::vector<T> &vector, std::vector<T> &out) {
+    out = vector;
+    rad2deg(vector);
+  }
 
 
 
@@ -75,215 +76,216 @@ namespace mathutils {
 //        return a * MU_180_PI;
 //    }
 
-    /* ===========================================================================================
-     * Velocities conversions functions
-     */
+  /* ===========================================================================================
+   * Velocities conversions functions
+   */
 
-    enum SPEED_UNIT {
-        MS,     ///> M/S
-        KNOT,   ///> NAUTICAL KNOTS
-        KMH     ///> KM/H
-    };
+  enum SPEED_UNIT {
+    MS,     ///> M/S
+    KNOT,   ///> NAUTICAL KNOTS
+    KMH     ///> KM/H
+  };
 
 
-    /// Convert nautical knots into m/s
-    template <class Real=double>
-    inline Real KNOT2MS(Real velocity) {
-        return velocity * MU_KNOT;
+  /// Convert nautical knots into m/s
+  template<class Real=double>
+  inline Real KNOT2MS(Real velocity) {
+    return velocity * MU_KNOT;
+  }
+
+  /// Convert m/s into nautical knots
+  template<class Real=double>
+  inline Real MS2KNOT(Real velocity) {
+    return velocity / MU_KNOT;
+  }
+
+  /// Convert km/h into m/s
+  template<class Real=double>
+  inline Real KMH2MS(Real velocity) {
+    return velocity * MU_KMH;
+  }
+
+  /// Convert m/s into km/h
+  template<class Real=double>
+  inline Real MS2KMH(Real velocity) {
+    return velocity / MU_KMH;
+  }
+
+  /// Convert km/h into nautical knots
+  template<class Real=double>
+  inline Real KMH2KNOT(Real velocity) {
+    return MS2KNOT(KMH2MS(velocity));
+  }
+
+  /// Convert nautical knots into km/h
+  template<class Real=double>
+  inline Real KNOT2KMH(Real velocity) {
+    return MS2KMH(KNOT2MS(velocity));
+  }
+
+  /// Conversion kt2ms inplace
+  template<class T>
+  inline void kt2ms(std::vector<T> &vector) {
+    for (auto &v : vector) v *= MU_KNOT;
+  }
+
+  /// CONVERSION kt2ms by copy
+  template<class T>
+  inline void kt2ms(const std::vector<T> &vector, std::vector<T> &out) {
+    out = vector;
+    return kt2ms(out);
+  }
+
+  /// Converts a velocity from a unit to another
+  template<class Real=double>
+  inline Real convert_velocity_unit(const Real velocity, SPEED_UNIT current_unit, SPEED_UNIT new_unit = MS) {
+
+    Real new_vel;
+    // Expressing in M/S
+    switch (current_unit) {
+      case MS:
+        new_vel = velocity;
+        break;
+      case KMH:
+        new_vel = KMH2MS(velocity);
+        break;
+      case KNOT:
+        new_vel = KNOT2MS(velocity);
     }
 
-    /// Convert m/s into nautical knots
-    template <class Real=double>
-    inline Real MS2KNOT(Real velocity) {
-        return velocity / MU_KNOT;
+    // EXPRESSING IN NEW UNIT
+    switch (new_unit) {
+      case MS:
+        return new_vel;
+      case KMH:
+        return MS2KMH(new_vel);
+      case KNOT:
+        return MS2KNOT(new_vel);
     }
 
-    /// Convert km/h into m/s
-    template <class Real=double>
-    inline Real KMH2MS(Real velocity) {
-        return velocity * MU_KMH;
+  }
+
+
+  /* ===========================================================================================
+   * Frequency conversion functions
+   */
+
+  enum FREQUENCY_UNIT {
+    HERTZ,  ///< Hertz (s**-1)
+    RADS,   ///< rad/s
+    DEGS,   ///< deg/s
+    DEGM,   ///< deg/min
+    S,      ///< seconds (period)
+    RPM     ///< Round Per Minute
+  };  // TODO: ajouter la conversion avec RPM ...
+
+  template<class Real=double>
+  inline Real DEGS2RADS(const Real degs) {
+    return degs * MU_PI_180;
+  }
+
+  template<class Real=double>
+  inline Real RADS2DEGS(const Real rads) {
+    return rads * MU_180_PI;
+  }
+
+
+  template<class Real=double>
+  inline Real HZ2RADS(const Real hz) {
+    return MU_2PI * hz;
+  }
+
+  template<class Real=double>
+  inline Real RADS2HZ(const Real rads) {
+    return rads / MU_2PI;
+  }
+
+  template<class Real=double>
+  inline Real HZ2S(const Real hz) {
+    return 1. / hz;
+  }
+
+  template<class Real=double>
+  inline Real S2HZ(const Real s) {
+    return 1. / s;
+  }
+
+  template<class Real=double>
+  inline Real RADS2S(const Real rads) {
+    return MU_2PI / rads;
+  }
+
+  template<class Real=double>
+  inline Real S2RADS(const Real s) {
+    return MU_2PI / s;
+  }
+
+  template<class Real=double>
+  inline Real RPM2RADS(const Real rpm) {
+    return rpm * MU_RPM2RADS;
+  }
+
+  template<class Real=double>
+  inline Real RADS2RPM(const Real rads) {
+    return rads / MU_RPM2RADS;
+  }
+
+  template<class Real=double>
+  inline Real DEGM2RADS(const Real degm) {
+    return degm * MU_PI_180 / 60;
+  }
+
+  template<class Real=double>
+  inline Real RADS2DEGM(const Real rads) {
+    return rads * MU_180_PI * 60;
+  }
+
+
+  template<class Real=double>
+  inline Real convert_frequency(const Real in, FREQUENCY_UNIT src_unit, FREQUENCY_UNIT target_unit) {
+
+    if (src_unit == target_unit) return in;
+
+    Real piv;  // Must be in Hz
+
+    switch (src_unit) {
+      case HERTZ:
+        piv = in;
+        break;
+      case RADS:
+        piv = RADS2HZ(in);
+        break;
+      case S:
+        piv = S2HZ(in);
+        break;
+      case DEGS:
+        piv = RADS2HZ(DEGS2RADS(in));
+        break;
+      case DEGM:
+        piv = RADS2HZ(DEGM2RADS(in));
+        break;
+      case RPM:
+        piv = RADS2HZ(RPM2RADS(in));
+        break;
     }
 
-    /// Convert m/s into km/h
-    template <class Real=double>
-    inline Real MS2KMH(Real velocity) {
-        return velocity / MU_KMH;
+    Real out;
+    switch (target_unit) {
+      case RADS:
+        out = HZ2RADS(piv);
+      case S:
+        out = HZ2S(piv);
+      case HERTZ:
+        out = piv;
+      case DEGS:
+        out = RADS2DEGS(HZ2RADS(piv));
+      case DEGM:
+        out = RADS2DEGM(HZ2RADS(piv));
+      case RPM:
+        out = RADS2RPM(HZ2RADS(piv));
     }
-
-    /// Convert km/h into nautical knots
-    template <class Real=double>
-    inline Real KMH2KNOT(Real velocity) {
-        return MS2KNOT(KMH2MS(velocity));
-    }
-
-    /// Convert nautical knots into km/h
-    template <class Real=double>
-    inline Real KNOT2KMH(Real velocity) {
-        return MS2KMH(KNOT2MS(velocity));
-    }
-
-    /// Conversion kt2ms inplace
-    template <class T>
-    inline void kt2ms(std::vector<T>& vector) {
-        for (auto& v : vector) v *= MU_KNOT;
-    }
-
-    /// CONVERSION kt2ms by copy
-    template <class T>
-    inline void kt2ms(const std::vector<T>& vector, std::vector<T>& out) {
-        out = vector;
-        return kt2ms(out);
-    }
-
-    /// Converts a velocity from a unit to another
-    template <class Real=double>
-    inline Real convert_velocity_unit(const Real velocity, SPEED_UNIT current_unit, SPEED_UNIT new_unit= MS) {
-
-        Real new_vel;
-        // Expressing in M/S
-        switch (current_unit) {
-            case MS:
-                new_vel = velocity;
-                break;
-            case KMH:
-                new_vel = KMH2MS(velocity);
-                break;
-            case KNOT:
-                new_vel = KNOT2MS(velocity);
-        }
-
-        // EXPRESSING IN NEW UNIT
-        switch (new_unit) {
-            case MS:
-                return new_vel;
-            case KMH:
-                return MS2KMH(new_vel);
-            case KNOT:
-                return MS2KNOT(new_vel);
-        }
-
-    }
-
-
-    /* ===========================================================================================
-     * Frequency conversion functions
-     */
-
-    enum FREQUENCY_UNIT {
-        HERTZ,  ///< Hertz (s**-1)
-        RADS,   ///< rad/s
-        DEGS,   ///< deg/s
-        DEGM,   ///< deg/min
-        S,      ///< seconds (period)
-        RPM     ///< Round Per Minute
-    };  // TODO: ajouter la conversion avec RPM ...
-
-    template <class Real=double>
-    inline Real DEGS2RADS(const Real degs) {
-        return degs * MU_PI_180;
-    }
-
-    template <class Real=double>
-    inline Real RADS2DEGS(const Real rads) {
-        return rads * MU_180_PI;
-    }
-
-
-    template <class Real=double>
-    inline Real HZ2RADS(const Real hz) {
-        return MU_2PI * hz;
-    }
-
-    template <class Real=double>
-    inline Real RADS2HZ(const Real rads) {
-        return rads / MU_2PI;
-    }
-
-    template <class Real=double>
-    inline Real HZ2S(const Real hz) {
-        return 1. / hz;
-    }
-
-    template <class Real=double>
-    inline Real S2HZ(const Real s) {
-        return 1. / s;
-    }
-
-    template <class Real=double>
-    inline Real RADS2S(const Real rads) {
-        return MU_2PI / rads;
-    }
-
-    template <class Real=double>
-    inline Real S2RADS(const Real s) {
-        return MU_2PI / s;
-    }
-
-    template <class Real=double>
-    inline Real RPM2RADS(const Real rpm) {
-        return rpm * MU_RPM2RADS;
-    }
-
-    template <class Real=double>
-    inline Real RADS2RPM(const Real rads) {
-        return rads / MU_RPM2RADS;
-    }
-
-    template <class Real=double>
-    inline Real DEGM2RADS(const Real degm) {
-        return degm * MU_PI_180 / 60;
-    }
-
-    template <class Real=double>
-    inline Real RADS2DEGM(const Real rads) {
-        return rads * MU_180_PI * 60;
-    }
-
-
-    template <class Real=double>
-    inline Real convert_frequency(const Real in, FREQUENCY_UNIT src_unit, FREQUENCY_UNIT target_unit) {
-
-        if (src_unit == target_unit) return in;
-
-        Real piv;  // Must be in Hz
-
-        switch (src_unit) {
-            case HERTZ:
-                piv = in;
-                break;
-            case RADS:
-                piv = RADS2HZ(in);
-                break;
-            case S:
-                piv = S2HZ(in);
-                break;
-            case DEGS:
-                piv = RADS2HZ(DEGS2RADS(in));
-                break;
-            case DEGM:
-                piv = RADS2HZ(DEGM2RADS(in));
-                break;
-            case RPM:
-                piv = RADS2HZ(RPM2RADS(in));
-                break;
-        }
-
-        switch (target_unit) {
-            case RADS:
-                return HZ2RADS(piv);
-            case S:
-                return HZ2S(piv);
-            case HERTZ:
-                return piv;
-            case DEGS:
-                return RADS2DEGS(HZ2RADS(piv));
-            case DEGM:
-                return RADS2DEGM(HZ2RADS(piv));
-            case RPM:
-                return RADS2RPM(HZ2RADS(piv));
-        }
-
-    }
+    return out;
+  }
 
 
 }
